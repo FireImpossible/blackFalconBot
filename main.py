8f0dc6c320ae384a84e14476acd400d4f44cb1a4
FireImpossible <58529129+FireImpossible@users.noreply.github.com>
2021-04-04 17:15:39 -0400

    did a thing

--- main.py
+++ main.py
@@ -9,46 +9,28 @@ import requests
 import time
 
 TOKEN = 'ODI4MzEzNTcyODUyNDk4NDUy.YGnxIQ.glzmYv3KgWJeYlizZMASQi2fuQs'
-
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 client = commands.Bot(command_prefix=['bf!', 'Bf!', 'bF!', 'BF!'], intents=intents)
 
-# webscraping lmao
-url = "https://www.uscyberpatriot.org/competition/current-competition/competition-schedule"
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-
-# title test
-title_id = html.find("<title>")
-start_title = title_id + len("<title>")
-end_title = html.find("</title>")
-title = html[start_title:end_title]
-
-# get exhibition round 1
-exe1_id = html.find("Exhibition Round 1")
-end_exe = exe1_id + len("Exhibition Round 1")
-exhibition1 = html[exe1_id:end_exe] + " " + str(exe1_id)
-#print(exhibition1)
-
-# soup
+# make the soup
 url = "https://www.uscyberpatriot.org/competition/current-competition/competition-schedule"
 page = urlopen(url)
 html = page.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 
+# bot starts
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     await client.get_channel(828314284441337889).send('I am alive')
-    #await client.get_channel(828314284441337889).send(exhibition1)
 
 # im keeping this
 @client.command()
 async def ping(ctx):
     await ctx.reply("pong!")
 
-@client.command()
+# gets comp dates
+@client.command(aliases=['comps', 'comp'])
 async def comps(ctx, *args):
 
     if not args:
@@ -65,8 +47,7 @@ async def comps(ctx, *args):
     competition_embed.set_thumbnail(url="https://www.kindpng.com/picc/m/136-1363669_afa-cyberpatriot-hd-png-download.png")
     competition_embed.set_footer(text=f"Viewing page {page_num} of 2.")
 
-    # page 1
-    if page_num == 1:
+    if page_num == 1: # page 1
         for x in range(3):
             mydivs = soup.find_all("tr", {"class": "ms-rteTableOddRow-default"})
             mystrong = mydivs[x].find_all("strong")
