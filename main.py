eecb05d31b6675ad05b08b2a14b0457c74628316
FireImpossible <58529129+FireImpossible@users.noreply.github.com>
2021-04-04 21:10:24 -0400

    made better :)

--- main.py
+++ main.py
@@ -12,11 +12,38 @@ TOKEN = 'ODI4MzEzNTcyODUyNDk4NDUy.YGnxIQ.glzmYv3KgWJeYlizZMASQi2fuQs'
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 client = commands.Bot(command_prefix=['bf!', 'Bf!', 'bF!', 'BF!'], intents=intents)
 
-# make the soup
-url = "https://www.uscyberpatriot.org/competition/current-competition/competition-schedule"
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
+# for the soup
+def fixText(text):
+    if text.find("(") != -1:
+        return text[0:text.find("(")]
+    return text
+def clearEmpty(array):
+    fixedArray = []
+    stringArray = []
+    for x in range(len(array)):
+        if array[x].get_text() != "":
+            if array[x].get_text() not in stringArray:
+                stringArray.append(array[x].get_text())
+                fixedArray.append(array[x])
+    return fixedArray
+def getEvents(Events):
+    eventObj = []
+    for x in range(len(Events)):
+        #print(Events[x].find("strong").get_text())
+        if(len(clearEmpty(Events[x].find_all("strong"))) > 0):
+            event = clearEmpty(Events[x].find_all("strong"))[0].get_text()
+            eventObj.append(fixText(event))
+            dateB = clearEmpty(Events[x].find_all("b"))
+            dateStrong = clearEmpty(Events[x].find_all("strong"))
+            if(len(dateStrong) < 2):
+                if(dateB):
+                    eventObj.append(dateB[0].get_text())
+                else:
+                    eventObj.append("not found")
+            else:
+                #print(dateStrong[1].get_text())
+                eventObj.append(dateStrong[1].get_text())
+    return eventObj
 
 # bot starts
 @client.event
@@ -32,7 +59,14 @@ async def ping(ctx):
 # gets comp dates
 @client.command(aliases=['comp'])
 async def comps(ctx, *args):
+    # remake the soup
+    url = "https://www.uscyberpatriot.org/competition/current-competition/competition-schedule"
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    tablebody = soup.find_all("tbody")
 
+    # get args length
     if not args:
         page_num = 1
     else:
@@ -41,46 +75,26 @@ async def comps(ctx, *args):
         else:
             page_num = int(args[0])
 
+    # make the embed
     competition_embed = discord.Embed(
         title=f"Competition Dates:"
     )
     competition_embed.set_thumbnail(url="https://www.kindpng.com/picc/m/136-1363669_afa-cyberpatriot-hd-png-download.png")
     competition_embed.set_footer(text=f"Viewing page {page_num} of 2.")
 
+    # flavor the soup
     if page_num == 1: # page 1
-        for x in range(3):
-            mydivs = soup.find_all("tr", {"class": "ms-rteTableOddRow-default"})
-            mystrong = mydivs[x].find_all("strong")
-            competition_embed.add_field(name=mystrong[0].get_text(), value=mystrong[-1].get_text(), inline=False)
-
-            if x == 0:
-                mydivs = soup.find_all("tr", {"class": "ms-rteTableEvenRow-default"})
-                mystrong = mydivs[1].find_all("strong")
-                competition_embed.add_field(name=mystrong[0].get_text(), value=mystrong[1].get_text(), inline=False)
-            elif x == 1:
-                mydivs = soup.find_all("tr", {"class": "ms-rteTableEvenRow-default"})
-                mystrong = mydivs[2].find_all("strong")
-                mybold = mydivs[2].find_all("b")
-                competition_embed.add_field(name=mystrong[0].get_text(), value=mybold[0].get_text(), inline=False)
-            elif x == 2:
-                mydivs = soup.find_all("tr", {"class": "ms-rteTableFooterRow-default"})
-                mystrong = mydivs[0].find_all("strong")
-                competition_embed.add_field(name=mystrong[0].get_text(), value=mystrong[-1].get_text(), inline=False)
+        trainEvents = tablebody[0].find_all("tr")
+        eventObj = getEvents(trainEvents)
+        for x in range(1, int(len(eventObj)/2)):
+            competition_embed.add_field(name=eventObj[x * 2], value=eventObj[(x * 2) + 1], inline=False)
     else: # page 2
-        for x in range(3, 5):
-            mydivs = soup.find_all("tr", {"class": "ms-rteTableOddRow-default"})
-            mystrong = mydivs[x].find_all("strong")
-            competition_embed.add_field(name=mystrong[0].get_text()[:7], value=mystrong[1].get_text(), inline=False)
+        roundEvents = tablebody[1].find_all("tr")
+        eventObj = getEvents(roundEvents)
+        for x in range(int(len(eventObj)/2)):
+            competition_embed.add_field(name=eventObj[x * 2], value=eventObj[(x * 2) + 1], inline=False)
 
-            if x == 3:
-                mydivs = soup.find_all("tr", {"class": "ms-rteTableEvenRow-default"})
-                mystrong = mydivs[x + 1].find_all("strong")
-                competition_embed.add_field(name=mystrong[0].get_text()[:7], value=mystrong[1].get_text(), inline=False)
-            else:
-                mydivs = soup.find_all("tr", {"class": "ms-rteTableFooterRow-default"})
-                mystrong = mydivs[1].find_all("strong")
-                competition_embed.add_field(name=mystrong[0].get_text(), value=mystrong[1].get_text(), inline=False)
-
+    # send the soup
     await ctx.send(embed=competition_embed)
 
 client.run(TOKEN)
