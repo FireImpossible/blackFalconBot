f6eb8e689d24b4d3b66b0d4d0390341ee888fa83
azn-abel <66392457+azn-abel@users.noreply.github.com>
2021-04-08 15:17:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ async def schedule(ctx, *args):
 
 def get_scheduled(guild):
 
-    cur.execute("SELECT * FROM announcements WHERE guildName = %s", (guild,))
+    cur.execute("SELECT * FROM announcements WHERE guildName = %s", (str(guild),))
     announce = cur.fetchall()
 
     sched_embed = discord.Embed(
