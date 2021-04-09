9cfb6f9f3d5d69a9c16ea26ed8c813cdac2f4d67
azn-abel <66392457+azn-abel@users.noreply.github.com>
2021-04-09 15:40:22 -0400

    final organization changes

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
-import os
 from bot import *
-from urllib.request import urlopen
 
-from bs4 import BeautifulSoup
-import grequests
-import requests 
-import time
-from soup_functions import *
-from database import *
-from badWords import bad_words
-from manage_timezones import *
-import psycopg2
-import datetime
-import asyncio
-
-import random
-import datetime
 INFO_DISPLAY_ENABLED = True
 EVENTS_ENABLED = True
 HELP_ENABLED = True
 LEADERSHIP_ENABLED = True
 MISCELLANEOUS_ENABLED = True
 
-# time_zone = 4
-# remove the time zone difference and implement convertDateTime()
+if INFO_DISPLAY_ENABLED:
+    from info_display import *
 
+if EVENTS_ENABLED:
+    from events import *
 
+if HELP_ENABLED:
+    from help import *
 
+if LEADERSHIP_ENABLED:
+    from leadership import *
+
+if MISCELLANEOUS_ENABLED:
+    from miscellaneous import *
 
 
 client.run(TOKEN)
