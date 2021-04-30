e9eceda1ccc352953dfafbcdd0df43990e7e99cf
azn-abel <66392457+azn-abel@users.noreply.github.com>
2021-04-30 09:21:09 -0400

    Update database.py

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ import os
 if os.getenv("HOME") != '/app':
     from environment import *
 
-DB_HOST = os.getenv('DATABASE_URL')[91:131]
-DB_NAME = os.getenv('DATABASE_URL')[137:]
-DB_USER = os.getenv('DATABASE_URL')[11:25]
-DB_PASS = os.getenv('DATABASE_URL')[26:90]
+DB_HOST = os.getenv('DATABASE_URL').split("/")[2].split(":")[1].split("@")[1]
+DB_NAME = os.getenv('DATABASE_URL').split("/")[-1]
+DB_USER = os.getenv('DATABASE_URL').split("/")[2].split(":")[0]
+DB_PASS = os.getenv('DATABASE_URL').split("/")[2].split(":")[1].split("@")[0]
 
 import psycopg2
 import psycopg2.extras
