18e0c525030e2c53912a9ca927eb734384ff0773
Kethly <57457270+Kethly@users.noreply.github.com>
2021-11-20 15:27:43 -0500

    Update info_display.py

--- info_display.py
+++ info_display.py
@@ -116,9 +116,6 @@ async def score(ctx, *args):
     score_embed.set_thumbnail(
         url="https://www.kindpng.com/picc/m/136-1363669_afa-cyberpatriot-hd-png-download.png")
     for key in teamResult.keys():
-        await ctx.send("testing")
         if teamResult[key] != "":
-            await ctx.send(key)
-            await ctx.send(teamResult[key])
-        #score_embed.add_field(name=key, value=teamResult[key], inline=False)
-    #await ctx.send(embed=score_embed)
+            score_embed.add_field(name=key, value=teamResult[key], inline=False)
+    await ctx.send(embed=score_embed)
